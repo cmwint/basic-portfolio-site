73001342ecd99dbbdca3ad23d2444dbf06611ef7
Corinne Winthrop <corinne.winthrop@gmail.com>
2017-04-13 23:16:52 -0600

    Add experience and portfolio

--- app/index.php
+++ app/index.php
@@ -34,16 +34,116 @@
                     
                 <div class="main-torso__inner">
                     <article id="skills" class="main-torso__group skills">
-                        skills section is here!
+                        <h2 class="skills__title">
+                            what I know
+                        </h2>
+                        <div class="skills__wrap">
+                            <div class="skills__col">
+                                <ul>
+                                    <li>HTML5, SASS, JavaScript, jQuery, PHP, AJAX</li>
+                                    <li>Content and database management systems: WordPress, Methode, Microsoft SQL Server</li>
+                                    <li>Git version control and command line tools</li>
+                                    <li>PHP and Javascript APIs</li>
+                                </ul>
+                            </div>
+                            <div class="skills__col">
+                                <ul>
+                                    <li>Familiarity with JS frameworks: AngularJS</li>
+                                    <li>Understanding of analytics and data visualizations, plus familiarity with D3.js</li>
+                                    <li>Customizing WP plugins based on client need</li>
+                                    <li>Adobe Creative Suite</li>
+                                </ul>
+                            </div>
+                        </div>
                     </article>
                     <article id="experience" class="main-torso__group portfolio">
-                        portfolio/experience section is here!
+                        <h2 class="portfolio__title">
+                            what I've done
+                        </h2>
+                        <div class="portfolio__wrap">
+                            <div class="portfolio__single">
+                                <div class="portfolio__job">
+                                    <h3>Zenman</h3>
+                                    <p>January 2015 - Present</p>
+                                </div>
+                                <div class="portfolio__description">
+                                    <h3>Senior Front-end Web Developer</h3>
+                                    <p>Led the development team in weekly code reviews, team meetings, and innovative front-end practices to keep the team moving forward</p>
+                                    <p>Mentored junior front-end web developers and assisted with code problems</p>
+
+                                    <h3>Front-end Web Developer</h3>
+                                    <p>Built custom WordPress sites from the ground up with a bare-bones theme</p>
+                                    <p>Developed a WordPress plugin to easily integrate social media feeds using the Facebook, Instagram, Twitter and Linkedin APIs</p>
+                                    <p>Researched front-end performance measures and how to improve them within WordPress websites</p>
+                                    <p>Worked with many CRM systems to integrate client forms and work ows with their websites</p>
+
+                                    <h3>Intern</h3>
+                                    <p>Devloped and maintained WordPress websites while managing their databases</p>
+                                    <p>Amplified skills in git, command line tools, PHP, SASS and JavaScript libraries</p>
+                                </div>
+                            </div>
+                            <div class="portfolio__single">
+                                <div class="portfolio__job">
+                                    <h3>agile42 - The Agile Coaching Company</h3>
+                                    <p>May 2016</p>
+                                </div>
+                                <div class="portfolio__description">
+                                    <h3>Certified Scrum Master Training</h3>
+                                </div>
+                            </div>
+                            <div class="portfolio__single">
+                                <div class="portfolio__job">
+                                    <h3>Galvanize Workshop</h3>
+                                    <p>March 2016 - April 2016</p>
+                                </div>
+                                <div class="portfolio__description">
+                                    <h3>Web Development with Javascript Foundations</h3>
+                                </div>
+                            </div>
+                            <div class="portfolio__single">
+                                <div class="portfolio__job">
+                                    <h3>Semantic Arts</h3>
+                                    <p>February 2015 - July 2015</p>
+                                </div>
+                                <div class="portfolio__description">
+                                    <h3>Intern</h3>
+                                    <p>Worked on various development projects with data and JSON<p>
+                                    <p>Assisted with light Python programming with the Flask microframework</p>
+                                    <p>Increased knowledge of SEO integration with code and Google Analytics practices</p>
+                                </div>
+                            </div>
+                            <div class="portfolio__single">
+                                <div class="portfolio__job">
+                                    <h3>The Boston Globe</h3>
+                                    <p>January 2014 - July 2014</p>
+                                </div>
+                                <div class="portfolio__description">
+                                    <h3>Editorial Design co-op</h3>
+                                    <p>Gained digital interactive design and development fundamentals from professional developers in the field</p>
+                                    <p>Designed sports graphics weekly from a set of data and photos to create data visualizations, while collaborating with veteran sports editors and established writers in the media industry</p>
+                                </div>
+                            </div>
+                            <div class="portfolio__single">
+                                <div class="portfolio__job">
+                                    <h3>Colorado State University</h3>
+                                    <p>Graduation Date December 2014</p>
+                                </div>
+                                <div class="portfolio__description">
+                                    <h3>Journalism major</h3>
+                                    <p>Minor Information Science & Technology</p>
+                                </div>
+                            </div>
+                        </div>
                     </article>
                     <article id="pocket-feed" class="main-torso__group pocket-feed">
-                        pocket feed via php here!
+                        <h2 class="pocket-feed__title">
+                            what I'm reading
+                        </h2>
                     </article>
                     <article id="learning" class="main-torso__group skills">
-                        another skills module, but tell what I'm learning!
+                        <h2 class="skills__title">
+                            what I've learning
+                        </h2>
                     </article>
                 </div>
 
